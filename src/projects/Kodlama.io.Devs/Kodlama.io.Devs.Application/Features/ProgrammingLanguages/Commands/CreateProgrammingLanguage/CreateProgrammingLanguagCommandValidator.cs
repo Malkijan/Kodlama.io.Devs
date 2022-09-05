5aef523fe1dd0c6cf7f3cfdaf6110dc5e5e51324
malkijan <ugurmalki@gmail.com>
2022-09-05 16:32:40 +0300

    3. Dersin eksik kalan konularına ait ödevler tamamlandı.

---
+++ src/projects/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguagCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguagCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguagCommandValidator()
+        {
+            RuleFor(pl => pl.Name).NotEmpty();
+        }
+    }
+}
--- src/projects/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Profiles/MappingProfiles.cs
+++ src/projects/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Profiles/MappingProfiles.cs
@@ -3,6 +3,7 @@ using Core.Persistence.Paging;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
 using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Models;
 using Kodlama.io.Devs.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,10 @@ namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Profiles
             CreateMap<ProgrammingLanguage, UpdateProgrammingLanguageCommand>().ReverseMap();
 
             CreateMap<ProgrammingLanguage, DeletedProgrammingLanguageDto>().ReverseMap();
+
+            CreateMap<ProgrammingLanguage, ProgrammingLanguageListDto>().ReverseMap();
+            CreateMap<IPaginate<ProgrammingLanguage>, ProgrammingLanguageListModel>().ReverseMap();
+            CreateMap<ProgrammingLanguage, ProgrammingLanguageGetByIdDto>().ReverseMap();
 
         }
     }
